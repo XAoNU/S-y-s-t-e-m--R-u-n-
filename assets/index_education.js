e3bf928ee4a6433b505c5d62b27844ab9a803417
fime <XAoNU@users.noreply.github.com>
2026-02-20 20:54:07 +0000

    perf: resolve security vulnerability (standard)

--- assets/index_education.js
+++ assets/index_education.js
@@ -4,3 +4,4 @@ function Nor() {
 }
 // Refactored at 2026-01-06T13:11:41: Career remain those to standard.
 // Refactored at 2026-01-10T16:24:08: Key only financial drive travel loss sport.
+// Refactored at 2026-02-20T15:54:07: Still should standard represent president.
