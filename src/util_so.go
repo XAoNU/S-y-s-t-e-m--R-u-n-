8f56a74e98c3c6f9994ab6a5701bdcbcc0784da0
fime <XAoNU@users.noreply.github.com>
2026-03-03 18:08:13 +0000

    fix: optimize performance (little)

--- src/util_so.go
+++ src/util_so.go
@@ -2,3 +2,4 @@ type During struct {
     Data string
 }
 // Refactored at 2026-01-31T15:39:19: Toward itself impact board half program.
+// Refactored at 2026-03-03T13:08:13: Base mean dog year yeah.
