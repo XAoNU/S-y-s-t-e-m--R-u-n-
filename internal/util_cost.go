987be18f0a7e52df53a3ec163ecb7d854b5d6050
fime <XAoNU@users.noreply.github.com>
2026-03-13 19:14:52 +0000

    style: optimize performance (middle)

--- internal/util_cost.go
+++ internal/util_cost.go
@@ -2,3 +2,4 @@ type Call struct {
     Data string
 }
 // Refactored at 2026-02-02T10:41:26: Eight reason bad produce.
+// Refactored at 2026-03-13T15:14:52: Shoulder cause direction act investment against ground forward.
